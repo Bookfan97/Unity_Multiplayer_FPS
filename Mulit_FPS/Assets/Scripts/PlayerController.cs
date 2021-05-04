897fc096bc74010a33dd2fd8e15bdf2dc5c86e12
Noah Day <sheltielover432@gmail.com>
2021-05-03 21:23:41 -0400

    Player Movement

--- Mulit_FPS/Assets/Scripts/PlayerController.cs
+++ Mulit_FPS/Assets/Scripts/PlayerController.cs
@@ -8,8 +8,10 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Transform viewPoint;
     [SerializeField] private float mouseSensitivty = 1.0f;
     [SerializeField] private bool shouldInverseMouse = false;
+    [SerializeField] private float moveSpeed = 5.0f;
     private float verticalRotationLimit;
     private Vector2 mouseInput;
+    private Vector3 moveDirection, movement;
     
     // Start is called before the first frame update
     void Start()
@@ -20,21 +22,40 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CameraMovement();
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        movement = ((transform.forward * moveDirection.z) + (transform.right * moveDirection.x)).normalized;
+        transform.position += movement * moveSpeed * Time.deltaTime;
+    }
+
+    private void CameraMovement()
+    {
         mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivty;
         transform.rotation = Quaternion.Euler(
-            transform.rotation.eulerAngles.x, 
+            transform.rotation.eulerAngles.x,
             transform.rotation.eulerAngles.y + mouseInput.x,
             transform.rotation.eulerAngles.z
-            );
+        );
         verticalRotationLimit += mouseInput.y;
+        verticalRotationLimit = Mathf.Clamp(verticalRotationLimit, -60f, 60f);
         if (shouldInverseMouse)
         {
-            verticalRotationLimit *= -1;
+            viewPoint.rotation = Quaternion.Euler(
+                verticalRotationLimit,
+                viewPoint.rotation.eulerAngles.y,
+                viewPoint.rotation.eulerAngles.z);
         }
-        verticalRotationLimit = Mathf.Clamp(verticalRotationLimit, -60f, 60f);
-        viewPoint.rotation = Quaternion.Euler(
-            verticalRotationLimit,
-            viewPoint.rotation.eulerAngles.y,
-            viewPoint.rotation.eulerAngles.z); 
+        else
+        {
+            viewPoint.rotation = Quaternion.Euler(
+                -verticalRotationLimit,
+                viewPoint.rotation.eulerAngles.y,
+                viewPoint.rotation.eulerAngles.z);
+        }
     }
 }
