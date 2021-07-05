85bd05e302f27d9a95d7d45323ffcc9d17170c6a
Noah Day <sheltielover432@gmail.com>
2021-07-05 00:38:51 -0400

    Connecting to Server

---
+++ Mulit_FPS/Assets/Scripts/Launcher.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using TMPro;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    public static Launcher instance;
+    public GameObject loadingScreen;
+    public GameObject menuButtons;
+    public TMP_Text loadingText;
+    
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CloseMenus();
+        loadingScreen.SetActive(true);
+        loadingText.text = "Connecting to Network...";
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    void CloseMenus()
+    {
+        loadingScreen.SetActive(false);
+        menuButtons.SetActive(false);
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+        loadingText.text = "Joining Lobby...";
+    }
+
+    public override void OnJoinedLobby()
+    {
+        CloseMenus();
+        menuButtons.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
