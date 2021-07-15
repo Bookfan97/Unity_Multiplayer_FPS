bac57294c04876f30bf9208814f9bad3a625c3cb
Noah Day <sheltielover432@gmail.com>
2021-07-14 22:06:49 -0400

    Setting up MatchManager

--- Mulit_FPS/Assets/Scripts/MatchManager.cs
+++ Mulit_FPS/Assets/Scripts/MatchManager.cs
@@ -1,14 +1,24 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using ExitGames.Client.Photon;
 using Photon.Pun;
+using Photon.Realtime;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MatchManager : MonoBehaviour
+public class MatchManager : MonoBehaviour, IOnEventCallback
 {
+    public enum EventCodes: byte
+    {
+        NewPlayer,
+        ListPlayers,
+        ChangeStat
+    }
     public static MatchManager instance;
-
+    public List<PlayerInfo> allPlayers = new List<PlayerInfo>();
+    public int index;
+    
     private void Awake()
     {
         instance = this;
@@ -26,6 +36,36 @@ public class MatchManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       
+    }
+
+    public void OnEvent(EventData photonEvent)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnEnable()
+    {
+        PhotonNetwork.AddCallbackTarget(this);
+    }
+
+    public void OnDisable()
+    {
+        PhotonNetwork.RemoveCallbackTarget(this);
+    }
+}
+
+[System.Serializable]
+public class PlayerInfo
+{
+    public string name;
+    public int actor, kills, deaths;
+
+    public PlayerInfo(string name, int actor, int kills, int deaths)
+    {
+        this.name = name;
+        this.actor = actor;
+        this.kills = kills;
+        this.deaths = deaths;
     }
 }
